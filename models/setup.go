68e641a3f3fe68b5dfa4b67f690bd5dce8dcd439
Muhammad Husni Nur Fadillah <muhammadhusninurfadillah@gmail.com>
2024-05-08 20:25:47 +0700

    fix model database config

--- models/setup.go
+++ models/setup.go
@@ -7,11 +7,11 @@ import (
 
 var DB *gorm.DB
 
-func ConnectDatabase(){
-	database, err:= gorm.Open(mysql.Open("root:@tcp(localhost:3306)/go_restapi_gin"))
+func ConnectDatabase() {
+	database, err := gorm.Open(mysql.Open("root:root@tcp(localhost:3306)/go_restapi_gin"))
 	if err != nil {
 		panic(err)
 	}
 	database.AutoMigrate(&Product{})
-	DB=database
+	DB = database
 }
